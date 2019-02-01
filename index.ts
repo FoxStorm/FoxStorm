e7199fac0ebd020b8ef9981c61ba48ee9d45b1a5
Sebastian Virlan <sebastian.virlan@bbc.co.uk>
2019-02-01 16:51:05 +0000

    Add new exports and update FoxStormServer

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 // Container related exports
-export { FoxStormContainer } from './src/container/FoxStormContainer'
-export { FoxStormServices } from './src/container/FoxStormServices'
-export { FoxStormConfig } from './src/container/FoxStormConfig'
-export { Environment } from 'foxstorm-container'
+export { FoxStormApplication } from './src/core/FoxStormApplication'
+export { FoxStormServices } from './src/core/FoxStormServices'
+export { FoxStormConfig } from './src/core/FoxStormConfig'
+export { FoxStormEnvironment } from './src/core/FoxStormEnvironment'
 
 // Framework Components
 export { BaseController } from './src/BaseController'
 export { BaseDecorator } from './src/BaseDecorator'
 export { BasePresenter } from './src/BasePresenter'
-export { Route, __invoke } from './src/Routing/Route'
-export { Request, Response } from './src/Http'
+// export { Route, __invoke } from './src/Routing/Route'
+export { Request, Response } from 'foxstorm-routing'
--- src/services/server/FoxStormServer.ts
+++ src/services/server/FoxStormServer.ts
@@ -1,12 +1,12 @@
 import { ApplicationServer } from 'foxstorm-server'
-import { FoxStormApplication } from '../../container/FoxStormApplication'
+import { FoxStormApplication } from '../../core/FoxStormApplication'
 import { FoxStormRouter } from '../router/FoxStormRouter'
 
 export class FoxStormServer extends ApplicationServer {
   static makeService (container: FoxStormApplication) {
-    // const router = container.retrieveServiceFor('Router')
-    // const logger = container.retrieveServiceFor('Logger')
-    // return new this(router, logger)
+    const router = container.retrieveServiceFor('Router') as FoxStormRouter
+    const logger = container.retrieveServiceFor('Logger')
+    return new this(router, logger)
   }
   constructor (readonly router: FoxStormRouter, readonly logger: any) {
     super()
